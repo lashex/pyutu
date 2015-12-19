7c65a0ce137e516d6f0b8c9ca133904e6f248488
Brett Francis <lashex@users.noreply.github.com>
2015-12-19 01:34:18 -0800

    ability to obtain prices for a service by region

--- pyutu/__init__.py
+++ pyutu/__init__.py
@@ -1,8 +1,138 @@
 # Copyright (c) 2015 Brett Francis http://www.oort.org
 #
 
-import os
+import os, sys
+import logging
+import requests
+import json
+from cachecontrol import CacheControl
+from cachecontrol.caches import FileCache
 
 
 __version__ = open(os.path.join(os.path.dirname(__file__), '_version')).read()
 
+
+logging.basicConfig(level=logging.DEBUG, stream=sys.stdout,
+                    format="%(asctime)s: " + logging.BASIC_FORMAT,
+                    datefmt="%Y-%m-%dT%H:%M:%S%z")
+logger = logging.getLogger(__name__)
+
+req = CacheControl(requests.Session(), cache=FileCache('pyutu.cache'))
+
+regions = {
+    'ap-northeast-1': "Asia Pacific (Tokyo)",
+    'ap-southeast-1': "Asia Pacific (Singapore)",
+    'ap-southeast-2': "Asia Pacific (Sydney)",
+    'eu-central-1': "EU (Frankfurt)",
+    'eu-west-1': "EU (Ireland)",
+    'sa-east-1': "South America (Sao Paulo)",
+    'us-east-1': "US East (N. Virginia)",
+    'us-west-1': "US West (N. California)",
+    'us-west-2': "US West (Oregon)"
+}
+
+
+svcs = {
+    "ec2": {
+        'offer_code': "AmazonEC2",
+        'prod_families': {
+            "Data Transfer": "fromLocation",
+            "Compute Instance": "location",
+            "IP Address": "location",
+            "Dedicated Host": "location"
+        }
+    },
+    "ses": {
+        'offer_code': "AmazonSES",
+        'prod_families': {
+            "Data Transfer": "fromLocation",
+            "Sending Email": "location"
+        }
+    },
+    "ddb": {
+        'offer_code': "AmazonDynamoDB",
+        'prod_families': {
+            "Data Transfer": "fromLocation",
+            "Database Storage": "location",
+            "Provisioned IOPS": "location"
+        }
+    },
+    "s3": {
+        'offer_code': "AmazonS3",
+        'prod_families': {
+            "Data Transfer": "fromLocation",
+            "API Request": "location",
+            "Storage": "location",
+            "Fee": "location"
+        }
+    }
+}
+#     "glacier": "AmazonGlacier",
+#     "cloudfront": "AmazonCloudFront",
+#     "cf": "AmazonCloudFront",
+#     "vpc": "AmazonVPC",
+#     "kms": "awskms",
+#     "rds": "AmazonRDS",
+#     "route53": "AmazonRoute53",
+#     "r53": "AmazonRoute53",
+#     "redshift": "AmazonRedshift",
+# }
+
+
+def check_service(svc):
+    if svc not in svcs:
+        raise ValueError('Invalid service: {0}'.format(svc))
+
+    return True
+
+
+def find_price(svc, region, offer_file, terms='OnDemand'):
+    check_service(svc=svc)
+    products = {}
+    for p in offer_file['products']:
+        product = offer_file['products'][p]
+        prod_fam = product['productFamily']
+        if prod_fam in svcs[svc]['prod_families']:
+            f2r = svcs[svc]['prod_families'][prod_fam]
+            attr_val = product['attributes'][f2r]
+            if attr_val == regions[region]:
+                sku = product['sku']
+                logger.debug('Found product SKU: {0} in region: {1}'.format(
+                    sku, region
+                ))
+                products[sku] = {
+                    'offerCode' : offer_file['offerCode'],
+                    'product': product,
+                    'term': offer_file['terms'][terms][sku]
+                }
+
+    return products
+
+
+class PricingContext(object):
+
+    def __init__(self, region, root="https://pricing.us-east-1.amazonaws.com"):
+        self.aws_root = root
+        self.region = region
+        self.aws_index = self.aws_root + "/offers/v1.0/aws/index.json"
+        self._aws_prepend = 'Amazon'
+        self.idx = req.get(self.aws_index).json()
+
+    def get_details(self):
+        logger.info("Publication Date: {0}".format(self.idx['publicationDate']))
+        logger.info("Format Version: {0}".format(self.idx['formatVersion']))
+
+    def get_prices(self, svc, terms='OnDemand'):
+        check_service(svc=svc)
+        service_alias = svcs[svc]['offer_code']
+        logger.info("Service Alias: {0}".format(service_alias))
+        url = self.aws_root + \
+              self.idx['offers'][service_alias]['currentVersionUrl']
+
+        logger.info("          URL: {0}".format(url))
+        logger.info("       Region: {0}".format(self.region))
+        offer_file = req.get(url).json()
+        prices = find_price(svc, self.region, offer_file)
+        logger.info("       Prices:{0}".format(json.dumps(
+            prices, indent=2, sort_keys=True))
+        )
--- pyutu/cli.py
+++ pyutu/cli.py
@@ -1,45 +1,7 @@
 #!/usr/bin/env python
 
 import click
-import requests
-
-
-aliases = {
-    "ec2": "AmazonEC2",
-    "ses": "AmazonSES",
-    "DynamoDB": "AmazonDynamoDB",
-    "ddb": "AmazonDynamoDB",
-    "glacier": "AmazonGlacier",
-    "cloudfront": "AmazonCloudFront",
-    "cf": "AmazonCloudFront",
-    "vpc": "AmazonVPC",
-    "kms": "awskms",
-    "rds": "AmazonRDS",
-    "route53": "AmazonRoute53",
-    "r53": "AmazonRoute53",
-    "redshift": "AmazonRedshift",
-    "s3": "AmazonS3",
-}
-
-
-class PricingContext(object):
-
-    def __init__(self, root="https://pricing.us-east-1.amazonaws.com"):
-        self.aws_root = root
-        self.aws_index = self.aws_root + "/offers/v1.0/aws/index.json"
-        self._aws_prepend = 'Amazon'
-        self.idx = requests.get(self.aws_index).json()
-
-    def get_details(self):
-        click.echo("Publication Date: {0}".format(self.idx['publicationDate']))
-        click.echo("Format Version: {0}".format(self.idx['formatVersion']))
-        click.echo("Offers: {0}".format(self.idx['offers']))
-
-    def get_price(self, svc):
-        click.echo("..just outputting for now...")
-        click.echo("Service Alias: {0}".format(aliases[svc]))
-        url = self.aws_root + self.idx['offers'][aliases[svc]]['currentVersionUrl']
-        click.echo("          URL: {0}".format(url))
+from pyutu import PricingContext
 
 
 pass_idx = click.make_pass_decorator(PricingContext, ensure=True)
@@ -49,7 +11,7 @@ pass_idx = click.make_pass_decorator(PricingContext, ensure=True)
 @click.option('--region', default='us-west-2')
 @click.pass_context
 def cli(ctx, region):
-    ctx.obj = PricingContext()
+    ctx.obj = PricingContext(region=region)
     # add a region directly until AWS CLI config integration
     ctx.obj.region = region
 
@@ -64,7 +26,7 @@ def index(idx):
 @click.argument('service')
 @pass_idx
 def price(idx, service):
-    idx.get_price(service)
+    idx.get_prices(service.lower())
 
 cli.add_command(index)
 cli.add_command(price)
