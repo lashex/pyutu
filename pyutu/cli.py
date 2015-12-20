62f387e3575601897d6397da02305ae4c3434f8f
Brett Francis <lashex@users.noreply.github.com>
2015-12-19 23:54:28 -0800

    shifted more emphasis to PricingContext

--- pyutu/cli.py
+++ pyutu/cli.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python
 
 import click
-from pyutu import PricingContext
+import pyutu
 
-
-pass_idx = click.make_pass_decorator(PricingContext, ensure=True)
+pass_pc = click.make_pass_decorator(pyutu.PricingContext, ensure=True)
 
 
 @click.group()
-@click.option('--region', default='us-west-2')
+@click.option('--region', default='us-west-2',
+              help='the region from which a price is to be determined')
 @click.pass_context
 def cli(ctx, region):
-    ctx.obj = PricingContext(region=region)
+    ctx.obj = pyutu.PricingContext(region=region)
     # add a region directly until AWS CLI config integration
     ctx.obj.region = region
 
 
 @cli.command()
-@pass_idx
-def index(idx):
-    idx.get_details()
+@pass_pc
+def index(pc):
+    pyutu.get_details(pc)
 
 
 @cli.command()
 @click.argument('service')
-@pass_idx
-def price(idx, service):
-    idx.get_prices(service.lower())
+@click.option('--sku', default=None, help='a SKU of a product within the given <SERVICE>')
+@pass_pc
+def price(pc, service, sku):
+    pc.sku = sku
+    pyutu.get_prices(pc, service.lower())
 
 cli.add_command(index)
 cli.add_command(price)
